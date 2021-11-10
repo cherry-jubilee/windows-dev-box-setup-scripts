45261d10cd10b8d314325031335e68fe88960b1d
youngmin-gwon <youngmin.gwon@gmail.com>
2021-11-10 18:09:31 +0900

    add custom tools list

---
+++ scripts/CustomDevTools.ps1
@@ -0,0 +1,19 @@
+# system and cli
+choco install -y curl                        --limit-output
+            
+# browsers             
+choco install -y GoogleChrome                --limit-output
+choco install -y Firefox                     --limit-output
+choco install -y microsoft-edge              --limit-output
+
+#fonts
+choco install sourcecodepro                  --limit-output
+
+# dev tools and frameworks
+choco install -y vscode                      --limit-output
+choco install -y microsoft-windows-terminal  --limit-output
+choco install -y everything                  --limit-output
+choco install -y ditto                       --limit-output
+choco install -y powertoys                   --limit-output          
+choco install -y dropbox                     --limit-output
+choco install -y slack                       --limit-output
