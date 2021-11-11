27550d308b28da42672e551c341025c5e2c42802
youngmin-gwon <youngmin.gwon@gmail.com>
2021-11-11 18:07:11 +0900

    add new choco tools

--- scripts/CustomDevTools.ps1
+++ scripts/CustomDevTools.ps1
@@ -1,3 +1,7 @@
+## Note 
+## packages below are checked available on 2021-11-11
+## (might be evoking errors on some packages later on)
+
 # system and cli
 choco install -y curl                        --limit-output
             
@@ -12,8 +16,17 @@ choco install sourcecodepro                  --limit-output
 # dev tools and frameworks
 choco install -y vscode                      --limit-output
 choco install -y microsoft-windows-terminal  --limit-output
-choco install -y everything                  --limit-output
-choco install -y ditto                       --limit-output
-choco install -y powertoys                   --limit-output          
+
+# productivity tools
+choco install -y libreoffice-fresh           --limit-output
 choco install -y dropbox                     --limit-output
+choco install -y everything                  --limit-output
+choco install -y powertoys                   --limit-output          
+choco install -y ditto                       --limit-output
+choco install -y dbeaver                     --limit-output
+
+# communication tools
 choco install -y slack                       --limit-output
+choco install -y discord                     --limit-output
+choco install -y gather                      --limit-output
+
