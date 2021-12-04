89aa405cecee53fd0ae4beff47d4a27eec172c51
youngmin-gwon <youngmin.gwon@gmail.com>
2021-12-04 17:51:06 +0900

    testing

--- scripts/WSLSetup.ps1
+++ scripts/WSLSetup.ps1
@@ -1,18 +1,17 @@
-$distro = "ubuntu1804"
 
 # download essential tools
-& $distro run apt-get install -y build-essential curl git wget make unzip zip g++ libtool xz-utils libglu1-mesa default-jdk
+ubuntu1804 run apt-get install -y build-essential curl git wget make unzip zip g++ libtool xz-utils libglu1-mesa default-jdk
 
 # download zsh
-& $distro run apt -y install zsh 
-& $distro run chsh -s $(which zsh)
+ubuntu1804 run apt -y install zsh 
+ubuntu1804 run chsh -s $(which zsh)
 
 # download oh-my-zsh
-& $distro run sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+ubuntu1804 run sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
 # echo "\n\nexport JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64\nexport PATH=\$PATH:\$JAVA_HOME/bin\n" >> ~/.zshrc
 
 # download oh-my-zsh theme (powerlevel10k)
-& $distro run git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ubuntu1804 run git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # download asdf
-& $distro run git clone https://github.com/asdf-vm/asdf.git ~/.asdf -y
+ubuntu1804 run git clone https://github.com/asdf-vm/asdf.git ~/.asdf -y
