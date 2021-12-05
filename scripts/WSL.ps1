6b45fd39e8353b37d78e0c920bc4d5ceb9f58366
youngmin-gwon <youngmin.gwon@gmail.com>
2021-12-06 00:15:42 +0900

    set back to default

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,8 +1,8 @@
 choco install -y wsl2
 
-#--- Ubuntu ---
+#--- Ubuntu --git p-
 # TODO: Move this to choco install once --root is included in that package
-choco install -y wsl-ubuntu-2004 # --params "/InstallRoot:true"
+choco install -y wsl-ubuntu-2004 --params "/InstallRoot:true"
 # run the distro once and have it install locally. The default account is "ubuntu:ubuntu".
 
 RefreshEnv
@@ -11,7 +11,6 @@ $distro = "ubuntu2004"
 & $distro run useradd -m "$username"
 if ($LASTEXITCODE -ne 0) { throw }
 & $distro run "echo '${username}:${password}' | chpasswd"
-& $distro run sh -c "echo '${username}:${password}' | sudo chpasswd" # wrapped in sh -c to get the pipe to work
 if ($LASTEXITCODE -ne 0) { throw }
 & $distro run chsh -s /bin/bash "$username"
 if ($LASTEXITCODE -ne 0) { throw }
