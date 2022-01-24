05695528d730e13a949c699386288147a613d473
youngmin-gwon <youngmin.gwon@gmail.com>
2022-01-24 10:15:03 +0900

    Settings: 추가 wsl, vscode 설정
    무엇을: wsl 설정을 위한 패키지 및 vscode 설정값 추가

--- scripts/ubuntu_setup/dev_env_setup.sh
+++ scripts/ubuntu_setup/dev_env_setup.sh
@@ -1,6 +1,6 @@
 # packages
-sudo apt-get install -y build-essential curl git wget make unzip  g++ libtool xz-utils  default-jdk net-tools  clang cmake pkg-config libgtk-3-dev
-        
+sudo apt-get install -y build-essential curl git wget make unzip  g++ libtool xz-utils  default-jdk net-tools  clang cmake pkg-config libgtk-3-dev libssl-dev zlib1g-dev
+
 # download zsh
 sudo apt -y install zsh 
 sudo chsh -s $(which zsh)
