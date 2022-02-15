b80554baffb601ce11393edd48a2380126fc98ec
youngmin-gwon <youngmin.gwon@gmail.com>
2022-02-15 20:59:07 +0900

    add setup for neovim

--- scripts/ubuntu_setup/dev_env_setup.sh
+++ scripts/ubuntu_setup/dev_env_setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y build-essential curl git wget make unzip  g++ libtool xz-utils  default-jdk net-tools  clang cmake pkg-config libgtk-3-dev libssl-dev zlib1g-dev
 
 # download zsh
-sudo apt -y install zsh 
+sudo apt -y install zsh exuberant-ctags neofetch neovim
 sudo chsh -s $(which zsh)
 
 # download zinit
